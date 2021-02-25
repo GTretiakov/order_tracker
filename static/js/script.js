72619c2f38e6ab87181d66092df62df20d8be4d9
GTretiakov <g.tret1990@gmail.com>
2021-02-25 05:15:43 +0000

    add datepicker and status dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
     $('.sidenav').sidenav({edge: 'right'});
+    $('.collapsible').collapsible();
+    $('.datepicker').datepicker({
+        showClearBtn: true
+    });
+    $('select').formSelect();
 });
